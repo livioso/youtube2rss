4c2e33d8552292a88e1e847d4077bce753f44ca6
Livio Bieri <livio@livio.li>
2016-10-23 12:37:50 +0200

    Improve discard handling of old files.

--- youtube2rss/main.py
+++ youtube2rss/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3.4
-
 from os import walk
 from xml.etree import ElementTree as et
 import youtube_dl
@@ -44,6 +43,7 @@ def get_metadata(filename):
 #   'status': 'finished',
 #   'total_bytes': 410304772
 # }
+#
 def progress_hook(progress, channel, downloads):
     if progress.get('status') == 'finished':
         filename_video = progress.get('filename')
@@ -66,21 +66,29 @@ def progress_hook(progress, channel, downloads):
 
 
 def download(channel, downloads):
-    # figure this out with youtube-dl -F => likely 22 (video/mp4)
+    verbose = channel.get("verbose_download_output")
     video_format = channel.get("preferred_video_format")
-
+    archive_file = get_download_archive_filepath(channel)
     within_range = youtube_dl.utils.DateRange(
         start=channel.get("download_videos_start_date"),
         end=channel.get("download_videos_end_date")
     )
 
+    # you can find all the options in the YoutubeDL.py file on Github, see:
+    # https://github.com/rg3/youtube-dl/blob/master/youtube_dl/YoutubeDL.py#L131-L291
+    #
     options = {
-        'format': video_format,
-        'daterange': within_range,
-        'ignoreerrors': True,           # can happen when format is not available, then just skip.
-        'writeinfojson': True,          # write the video description to .info.json.
-        'nooverwrites': True,           # prevent overwriting files.
-        'progress_hooks': [lambda progress: progress_hook(progress, channel, downloads)]
+        'quiet': not verbose,                   # do not print messages to standard out.
+        'format': video_format,                 # figure out with youtube-dl -F => likely 22 (video/mp4).
+        'download_archive': archive_file,       # file that tracks downloads, videos present in file are not downloaded again.
+        'daterange': within_range,              # date range of videos we are going to download.
+        'ignoreerrors': True,                   # can happen when format is not available, then just skip.
+        'writeinfojson': True,                  # write the video description to .info.json.
+        'nooverwrites': True,                   # prevent overwriting files if we have them.
+        'progress_hooks': [
+            lambda progress:
+                progress_hook(progress, channel, downloads)
+        ]
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
@@ -91,7 +99,7 @@ def download(channel, downloads):
     return downloads
 
 
-def processing_file(channel):
+def get_processing_filepath(channel):
     rss = channel.get("rss")
     rss_file = rss.get("feed_output_file_name")
     return "{rss_file}.json".format(
@@ -99,14 +107,23 @@ def processing_file(channel):
     )
 
 
-def readProcessingFile(channel):
-    with open(processing_file(channel), 'r') as input_file:
-        return json.load(input_file)
-    return []
+def get_download_archive_filepath(channel):
+    username = channel.get("username")
+    return "{username}_download_archive".format(
+        username=username
+    )
 
 
-def writeProcessingFile(channel, downloads):
-    with open(processing_file(channel), 'w') as output_file:
+def read_processing_file(channel):
+    try:
+        with open(get_processing_filepath(channel), 'r') as input_file:
+            return json.load(input_file)
+    except:
+        return []  # no such file, or whatever
+
+
+def write_processing_file(channel, downloads):
+    with open(get_processing_filepath(channel), 'w') as output_file:
         json.dump(downloads, output_file)
 
 
@@ -134,7 +151,7 @@ def build_rss_episode_item(video, channel):
     enclosure.set('length', str(metadata.get('filesize')))
     enclosure.set('type', 'video/mp4')
 
-    # pubDate is a bit clowny. The expected format is: Wed, 03 Nov 2015 19:18:00 GMT
+    # pubDate is a bit clowny. The expected format is: Wed, 03 Nov 2015 19:18:00 GMT 🙄
     upload_date = datetime.datetime.strptime(metadata.get("upload_date"), "%Y%m%d").date()
     upload_date_formated = upload_date.strftime("%a, %d %b %Y 12:00:00 GMT")
 
@@ -201,12 +218,16 @@ def discard_old_files(channel, downloads):
     files_to_keep = []     # these are the good guys
     files_to_discard = []  # these are the bad guys!
 
-    # keep: the feed itself
+    # keep: the feed itself .rss
     rss_feed_file = channel.get("rss").get("feed_output_file_name")
     files_to_keep.append(rss_feed_file)
 
-    # keep: the processing file
-    files_to_keep.append(processing_file(channel))
+    # keep: the download_archive file
+    archive_file = get_download_archive_filepath(channel)
+    files_to_keep.append(archive_file)
+
+    # keep: the processing file .rss.json
+    files_to_keep.append(get_processing_filepath(channel))
 
     # keep: all (in this cycle) downloaded videos & meta data
     for download in downloads:
@@ -220,85 +241,21 @@ def discard_old_files(channel, downloads):
         files_to_discard.extend(filenames)
         break
 
-    # discard: files_to_discard XOR files_to_keep
-    files_to_discard = list(set(files_to_discard) ^ set(files_to_keep))
+    # discard: files_to_discard without files_to_keep
+    files_to_discard = list(set(files_to_discard) - set(files_to_keep))
+    print(files_to_keep)
+    print(files_to_discard)
     for file_to_discard in files_to_discard:
         print("  ➟ Discarding {filename}".format(filename=file_to_discard))
         os.remove(file_to_discard)
 
+
 def main():
-    caseyneistat = {
-        "username": "caseyneistat",
-        "preferred_video_format": "22",                             # video/mp4 => see youtube-dl -F.
-        "download_videos_start_date": "today-7day",                 # should be fine, since yesterday.
-        "download_videos_end_date": "today",                        # cron job runs frequently anyway.
-        "file_usage_quota_gb": 2,
-        "rss": {                                                    # mostly irrelevant, just required
-            "title": "Livioso - Casey Neistat",                     # for a minimal viable set of tags.
-            "image": "goo.gl/VkvYQN",                               # FIXME used as podcast image
-            "description": "Casey's Vlogs",                         # ... whatever (but required)
-            "link": "https://www.youtube.com/user/caseyneistat",    # ... whatever (^)
-            "author": "😎",                                         # ... whatever (^)
-            "base_url": "http://livio.li/podcasts/yt/",             # where the files will accessible
-            "feed_output_file_name": "feed-casey.rss"               # is under base_url
-        }
-    }
-
-    johnoliver = {
-        "username": "LastWeekTonight",
-        "preferred_video_format": "22",
-        "download_videos_start_date": "today-7day",
-        "download_videos_end_date": "today",
-        "file_usage_quota_gb": 2,
-        "rss": {
-            "title": "Livioso - Last Week Tonight",
-            "image": "goo.gl/VkvYQN",                              # FIXME
-            "description": "Last Week Tonight",
-            "link": "https://www.youtube.com/user/LastWeekTonight",
-            "author": "😎",
-            "base_url": "http://livio.li/podcasts/yt/",
-            "feed_output_file_name": "feed-lwt.rss"
-        }
-    }
-
-    chromedevelopers = {
-        "username": "ChromeDevelopers",
-        "preferred_video_format": "22",
-        "download_videos_start_date": "today-7day",
-        "download_videos_end_date": "today",
-        "file_usage_quota_gb": 2,
-        "rss": {
-            "title": "Livioso - Chrome Developers",
-            "image": "goo.gl/VkvYQN",                              # FIXME
-            "description": "Chrome Developers",
-            "link": "https://www.youtube.com/user/ChromeDevelopers",
-            "author": "😎",
-            "base_url": "http://livio.li/podcasts/yt/",
-            "feed_output_file_name": "feed-cd.rss"
-        }
-    }
-
-    quickybaby = {
-        "username": "QuickyBabyTV",
-        "preferred_video_format": "22",
-        "download_videos_start_date": "today-7day",
-        "download_videos_end_date": "today",
-        "file_usage_quota_gb": 2,
-        "rss": {
-            "title": "Livioso - QB",
-            "image": "goo.gl/VkvYQN",                              # FIXME
-            "description": "QB",
-            "link": "https://www.youtube.com/user/QuickyBabyTV",
-            "author": "😎",
-            "base_url": "http://livio.li/podcasts/yt/",
-            "feed_output_file_name": "feed-qb.rss"
-        }
-    }
-
     r00k123123 = {
         "username": "r00k123123",
+        "verbose_download_output": True,
         "preferred_video_format": "22",
-        "download_videos_start_date": "today-1year",
+        "download_videos_start_date": "today-6year",
         "download_videos_end_date": "today",
         "file_usage_quota_gb": 2,
         "rss": {
@@ -327,7 +284,7 @@ def main():
     for channel in channels_to_download:
 
         # keep track of previous downloads
-        downloads = readProcessingFile(channel)
+        downloads = read_processing_file(channel)
 
         print("🌍  Downloading channel {username}..."
               .format(username=channel.get("username")))
@@ -339,8 +296,76 @@ def main():
         print("🗑  Discarding old files...")
         discard_old_files(channel, downloads)
 
-        writeProcessingFile(channel, downloads)
+        write_processing_file(channel, downloads)
 
         print("🍹  Finalizing... Done.")
 
 main()
+
+#     caseyneistat = {
+#         "username": "caseyneistat",
+#         "preferred_video_format": "22",                             # video/mp4 => see youtube-dl -F.
+#         "download_videos_start_date": "today-7day",                 # should be fine, since yesterday.
+#         "download_videos_end_date": "today",                        # cron job runs frequently anyway.
+#         "file_usage_quota_gb": 2,
+#         "rss": {                                                    # mostly irrelevant, just required
+#             "title": "Livioso - Casey Neistat",                     # for a minimal viable set of tags.
+#             "image": "goo.gl/VkvYQN",                               # FIXME used as podcast image
+#             "description": "Casey's Vlogs",                         # ... whatever (but required)
+#             "link": "https://www.youtube.com/user/caseyneistat",    # ... whatever (^)
+#             "author": "😎",                                         # ... whatever (^)
+#             "base_url": "http://livio.li/podcasts/yt/",             # where the files will accessible
+#             "feed_output_file_name": "feed-casey.rss"               # is under base_url
+#         }
+#     }
+
+#     johnoliver = {
+#         "username": "LastWeekTonight",
+#         "preferred_video_format": "22",
+#         "download_videos_start_date": "today-7day",
+#         "download_videos_end_date": "today",
+#         "file_usage_quota_gb": 2,
+#         "rss": {
+#             "title": "Livioso - Last Week Tonight",
+#             "image": "goo.gl/VkvYQN",                              # FIXME
+#             "description": "Last Week Tonight",
+#             "link": "https://www.youtube.com/user/LastWeekTonight",
+#             "author": "😎",
+#             "base_url": "http://livio.li/podcasts/yt/",
+#             "feed_output_file_name": "feed-lwt.rss"
+#         }
+#     }
+
+#     chromedevelopers = {
+#         "username": "ChromeDevelopers",
+#         "preferred_video_format": "22",
+#         "download_videos_start_date": "today-7day",
+#         "download_videos_end_date": "today",
+#         "file_usage_quota_gb": 2,
+#         "rss": {
+#             "title": "Livioso - Chrome Developers",
+#             "image": "goo.gl/VkvYQN",                              # FIXME
+#             "description": "Chrome Developers",
+#             "link": "https://www.youtube.com/user/ChromeDevelopers",
+#             "author": "😎",
+#             "base_url": "http://livio.li/podcasts/yt/",
+#             "feed_output_file_name": "feed-cd.rss"
+#         }
+#     }
+
+#     quickybaby = {
+#         "username": "QuickyBabyTV",
+#         "preferred_video_format": "22",
+#         "download_videos_start_date": "today-7day",
+#         "download_videos_end_date": "today",
+#         "file_usage_quota_gb": 2,
+#         "rss": {
+#             "title": "Livioso - QB",
+#             "image": "goo.gl/VkvYQN",                              # FIXME
+#             "description": "QB",
+#             "link": "https://www.youtube.com/user/QuickyBabyTV",
+#             "author": "😎",
+#             "base_url": "http://livio.li/podcasts/yt/",
+#             "feed_output_file_name": "feed-qb.rss"
+#         }
+#     }
