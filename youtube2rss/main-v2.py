365fc63542d275328a4d8c7bbd2f655519086303
Livio Bieri <livio@livio.li>
2017-05-31 15:54:57 +0200

    WiP: Experiment with embedded url

--- youtube2rss/main-v2.py
+++ youtube2rss/main-v2.py
@@ -7,38 +7,68 @@ from requests.exceptions import RequestException
 from youtube_dl.utils import UnavailableVideoError, MaxDownloadsReached
 from youtube_dl import YoutubeDL
 
+class RSSFeedBuilder(object)
+
+    def __init__(self, videos):
+        self.videos = videos
+
+    def build_rss_feed(channel_info, channel_downloads):
+        root = build_rss_root(channel_info)
+        channel = root.find('channel')
+
+        # append episodes aka 'items'
+        for video in channel_downloads:
+            item = build_rss_episode_item(video, channel_info)
+            channel.append(item)
+
+        write_rss_feed(channel_info, root)
 
 class Youtube2Rss(object):
 
     def build_feed(self, channel_id):
-        options = {}
+        videos = Youtube2Rss.build_videos_dict(channel_id)
 
-        with YoutubeDL(options) as ydl:
+    @staticmethod
+    def build_rss_feed(channel_info, channel_downloads):
+        root = build_rss_root(channel_info)
+        channel = root.find('channel')
 
-            videos = self.fetch_playlist_videos(channel_id)
+        # append episodes aka 'items'
+        for video in channel_downloads:
+            item = build_rss_episode_item(video, channel_info)
+            channel.append(item)
 
-            for _, (video, snippet) in enumerate(videos.items()):
-
-                try:
-                    video_info = ydl.extract_info(video, download=False)
-
-                except UnavailableVideoError:
-                    raise
-
-                except MaxDownloadsReached:
-                    raise
-
-    def fetch_playlist_videos(self, playlist_id):
-
-        def snippet(item):
-            return item.get('snippet', {})
-
-        def video_id(item):
-            return (snippet(item).get('resourceId', {})
-                                 .get('videoId'))
-
+    @staticmethod
+    def build_videos_dict(channel_id):
         videos = {}
 
+        for snippet in Youtube2Rss.fetch_playlist_videos(channel_id):
+            vid = (snippet.get('resourceId', {})
+                          .get('videoId'))
+
+            info = Youtube2Rss.fetch_video_info(vid)
+
+            # build <key, value> dict where
+            # key = video id and value = snippet & info
+            videos[vid] = {'snippet': snippet, 'info': info}
+
+        return videos
+
+    @staticmethod
+    def fetch_video_info(self, video_id):
+        try:
+            options = {}
+            with YoutubeDL(options) as ydl:
+                return ydl.extract_info(video_id, download=False)
+
+        except UnavailableVideoError:
+            print('Video is unavailable')
+
+        except MaxDownloadsReached:
+            print('Reached the maximum of downloads.')
+
+    @staticmethod
+    def fetch_playlist_videos(self, playlist_id):
         try:
             response = requests.get(
                 url="https://www.googleapis.com/youtube/v3/playlistItems",
@@ -51,17 +81,10 @@ class Youtube2Rss(object):
             )
 
             data = response.json()
-
-            import ipdb
-            ipdb.set_trace()
-
-            # build <key, value> dict where key = video_id and value = snippet
-            videos = {video_id(item): snippet(item) for item in data.get('items', [])}
+            return [item['snippet'] for item in data['items']]
 
         except RequestException:
             print('HTTP Request failed')
-
-        return videos
 
 
 if __name__ == '__main__':
