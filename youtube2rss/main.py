129e58af0ceeafb97507c275807625fd2d9f202e
Livio Bieri <livioso@users.noreply.github.com>
2017-09-04 21:14:47 +0200

    Minor Cleanup (#5)
    * Use logger, fixes python comments
    
    * Crash hard if data is missing.
    
    Stuff that must be there should not be accessed with .get() but
    rather with [] because that way it will crash earlier.

--- youtube2rss/main.py
+++ youtube2rss/main.py
@@ -1,21 +1,26 @@
-#!/usr/bin/env python3.4
 from os import walk
 from xml.etree import ElementTree as et
 import youtube_dl
 import datetime
 import hashlib
+import logging
 import urllib
 import json
 import sys
 import os
 
+logger = logging.getLogger(__name__)
 
-# download a channel and return array of downloaded videos.
-# channel (options) are described in the channel_file
-# => see read_channel_file for more.
+
 def download(channel, downloads):
+    '''
+    Download a channel and return list of downloaded videos.
+    Channel (options) are described in the channel_file
+    → see read_channel_file for more.
+    '''
+
     verbose = channel.get('verbose_output', False)
-    download_settings = channel.get('download')
+    download_settings = channel.get('download', {})
     archive_file = get_download_archive_filepath(channel)
     within_range = youtube_dl.utils.DateRange(
         start=download_settings.get('from', None),
@@ -39,8 +44,8 @@ def download(channel, downloads):
         ]
     }
 
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download([channel.get('channel')])
+    with youtube_dl.YoutubeDL(options) as downloader:
+        downloader.download([channel['channel']])
 
     # sort the video by upload_date, given as a string => YYYYMMDD.
     downloads.sort(key=lambda video: video.get('metadata', {}).get('upload_date'))
@@ -48,18 +53,20 @@ def download(channel, downloads):
     return downloads
 
 
-# progress hook that gets called during download =>
-# this callback gets progress that is defined like this:
-# {
-#   '_total_bytes_str': '391.30MiB',
-#   'filename': 'five million-UqADuUBMoZ4.mp4',
-#   'status': 'finished',
-#   'total_bytes': 410304772
-# }
-#
 def progress_hook(progress, channel, downloads):
+    '''
+    Progress hook that gets called during download
+    → this callback gets progress that is defined like this:
+    {
+      '_total_bytes_str': '391.30MiB',
+      'filename': 'five million-UqADuUBMoZ4.mp4',
+      'status': 'finished',
+      'total_bytes': 410304772
+    }
+    '''
+
     if progress.get('status') == 'finished':
-        filename_video = progress.get('filename')
+        filename_video = progress['filename']
         base_filename = os.path.splitext(filename_video)[0]
 
         # the meta data file is stored with the extension .info.json
@@ -78,9 +85,12 @@ def progress_hook(progress, channel, downloads):
         downloads.append(video)
 
 
-# get the meta data we care about there is much more
-# (different formats etc.) but we do not need it anyways.
 def get_metadata(filename):
+    '''
+    Get the meta data we care about there is much more
+    (different formats etc.) but we do not need it anyways.
+    '''
+
     with open(filename) as metadata_file:
         video_info = json.load(metadata_file)
 
@@ -103,7 +113,6 @@ def get_metadata(filename):
         return metadata
 
 
-# build the RSS feed & write it to feed_output_file_name.
 def build_rss_feed(channel_info, channel_downloads):
     root = build_rss_root(channel_info)
     channel = root.find('channel')
@@ -113,59 +122,57 @@ def build_rss_feed(channel_info, channel_downloads):
         item = build_rss_episode_item(video, channel_info)
         channel.append(item)
 
-    write_rss_feed(channel_info, root)
+    return root
 
 
-# build the minimal root // channel RSS feed.
 def build_rss_root(channel_info):
     root = et.Element('rss')
     root.set('version', '2.0')
     root.set('xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd')
     channel = et.SubElement(root, 'channel')
-    rss = channel_info.get('rss')
+    rss = channel_info['rss']
 
     # add minimal set of elements to channel
     title = et.SubElement(channel, 'title')
-    title.text = rss.get('title')
+    title.text = rss['title']
 
     itunes_image = et.SubElement(channel, 'itunes:image')
-    itunes_image.set('href', rss.get('image'))
+    itunes_image.set('href', rss['image'])
 
     itunes_author = et.SubElement(channel, 'itunes:author')
-    itunes_author.text = rss.get('author')
+    itunes_author.text = rss['author']
 
     link = et.SubElement(channel, 'link')
-    link.text = rss.get('link')
+    link.text = rss['link']
 
     description = et.SubElement(channel, 'description')
-    description.text = rss.get('description')
+    description.text = rss['description']
 
     return root
 
 
-# build the minimal item (aka episodes):
 def build_rss_episode_item(video, channel):
-    metadata = video.get('metadata')
+    metadata = video['metadata']
 
     # build item with the minimal set of tags
     item = et.Element('item')
     title = et.SubElement(item, 'title')
-    title.text = metadata.get('title')
+    title.text = metadata['title']
 
     link = et.SubElement(item, 'link')
-    link.text = metadata.get('webpage_url')
+    link.text = metadata['webpage_url']
 
     description = et.SubElement(item, 'description')
-    description.text = metadata.get('description')
+    description.text = metadata['description']
 
     enclosure_url = '{base_url}{filename}'.format(
-        base_url=channel.get('rss').get('feed_base_url'),
-        filename=video.get('filename_video_url')
+        base_url=channel['rss']['feed_base_url'],
+        filename=video['filename_video_url']
     )
 
     enclosure = et.SubElement(item, 'enclosure')
     enclosure.set('url', enclosure_url)
-    enclosure.set('length', str(metadata.get('filesize')))
+    enclosure.set('length', str(metadata['filesize']))
     enclosure.set('type', 'video/mp4')
 
     # pubDate is a bit clowny. The expected format is: Wed, 03 Nov 2015 19:18:00 GMT 🙄
@@ -179,8 +186,8 @@ def build_rss_episode_item(video, channel):
 
 
 def write_rss_feed(channel_info, root):
-    rss = channel_info.get('rss')
-    rss_file = rss.get('feed_output_file_name')
+    rss = channel_info['rss']
+    rss_file = rss['feed_output_file_name']
 
     root_et = et.ElementTree(root)
     root_et.write(
@@ -190,14 +197,17 @@ def write_rss_feed(channel_info, root):
     )
 
 
-# delete files that are not used anymore (see keep_latest)
-# does not delete .rss, .rss.json and download_archive.
 def discard_old_downloads(channel, downloads):
+    '''
+    Delete files that are not used anymore (only keep_latest)
+    does not delete .rss, .rss.json and download_archive!
+    '''
+
     files_to_keep = []     # these are the good guys
     files_to_discard = []  # these are the bad guys!
 
     # keep: the feed itself .rss
-    rss_feed_file = channel.get('rss').get('feed_output_file_name')
+    rss_feed_file = channel['rss']['feed_output_file_name']
     files_to_keep.append(rss_feed_file)
 
     # keep: the download_archive file
@@ -208,10 +218,10 @@ def discard_old_downloads(channel, downloads):
     files_to_keep.append(get_processing_filepath(channel))
 
     # keep: latest downloads, adjust with download.keep_latest
-    keep_latest = channel.get('download').get('keep_latest')
+    keep_latest = channel['download']['keep_latest']
     for download in downloads[-keep_latest:]:
-        metadata_file = download.get('metadata').get('_metadata_filename')
-        video_file = download.get('metadata').get('_video_filename')
+        metadata_file = download['metadata']['_metadata_filename']
+        video_file = download['metadata']['_video_filename']
         files_to_keep.append(metadata_file)
         files_to_keep.append(video_file)
 
@@ -223,14 +233,12 @@ def discard_old_downloads(channel, downloads):
     # discard: files_to_discard without files_to_keep
     files_to_discard = list(set(files_to_discard) - set(files_to_keep))
     for file_to_discard in files_to_discard:
-        print('  ➟ Discarding {filename}'.format(filename=file_to_discard))
+        logger.debug('  ➟ Discarding {filename}'.format(filename=file_to_discard))
         os.remove(file_to_discard)
 
 
-# filename of download_archive (an option by YoutubeDL).
 def get_download_archive_filepath(channel):
-    channel = channel.get('channel')
-
+    channel = channel['channel']
     # use hash as a valid channel can also be a channel url
     channel_hash = hashlib.md5(channel.encode('utf-8')).hexdigest()[:10]
 
@@ -239,10 +247,9 @@ def get_download_archive_filepath(channel):
     )
 
 
-# filename of our own processing file (dump of downloads)
 def get_processing_filepath(channel):
-    rss = channel.get('rss')
-    rss_file = rss.get('feed_output_file_name')
+    rss = channel['rss']
+    rss_file = rss['feed_output_file_name']
     return '{rss_file}.json'.format(
         rss_file=rss_file
     )
@@ -261,11 +268,13 @@ def read_processing_file(channel):
         return []  # no such file, or whatever
 
 
-# read a channel from json for the required
-# structure see casey.json
 def read_channel_file(argv):
+    '''
+    Read a channel from json for the required structure.
+    '''
+
     if len(argv) != 2:
-        print(' ➟ Aborting. Please specify a channel json as parameter.')
+        logger.error(' ➟ Aborting. Please specify a channel json as parameter.')
         sys.exit(1)
 
     # we only have this one parameter
@@ -275,28 +284,34 @@ def read_channel_file(argv):
         with open(channel_json_filepath, 'r') as input_file:
             return json.load(input_file)
     except:
-        print(' ➟ Aborting. Can not parse file {filename}'.format(
+        logger.error(' ➟ Aborting. Can not parse file {filename}'.format(
             filename=channel_json_filepath
         ))
         sys.exit(1)
 
 
 def main():
-    print('⏳  Preparing channel download...')
+    logging.basicConfig(level=logging.DEBUG)
+
+    logger.info(' ⏳  Preparing channel download...')
     channel = read_channel_file(sys.argv)
     downloads = read_processing_file(channel)
 
-    print('🌍  Downloading channel {channel}...'
-          .format(channel=channel.get('channel')))
+    logger.info(' 🌍  Downloading channel {channel}...'.format(
+        channel=channel['channel']))
     downloads = download(channel, downloads)
 
-    print('⚙  Building RSS Feed...')
-    build_rss_feed(channel, downloads)
+    logger.info(' ⚙  Building RSS Feed...')
+    rss_feed = build_rss_feed(channel, downloads)
+    write_rss_feed(channel, rss_feed)
 
-    print('🗑  Cleaning up...')
+    logger.info(' 🗑  Cleaning up...')
     discard_old_downloads(channel, downloads)
     write_processing_file(channel, downloads)
 
-    print('🍹  Finalizing... Done.')
+    logger.info(' 🍹  Finalizing... Done.')
 
-main()  # let's rock 🐣
+
+if __name__ == "__main__":
+    main()
+
